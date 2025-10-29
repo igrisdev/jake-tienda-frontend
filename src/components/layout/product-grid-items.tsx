448cde387acf60d49e22b4c7bf44ed14686d97bf
igrisdev <johanalvarezz237@gmail.com>
2025-10-28 22:34:15 -0500

    Añadir indicador de "Agotado" para productos no disponibles en el componente ProductCard

--- src/components/layout/product-grid-items.tsx
+++ src/components/layout/product-grid-items.tsx
@@ -39,6 +39,11 @@ export const ProductCard = ({ product }: { product: Product }) => {
             priority
             // unoptimized
           /> */}
+          {!product.availableForSale && (
+            <div className="absolute top-0 left-0 z-10 flex items-center justify-center rounded-full bg-black/80 px-2 py-1 text-xs text-white backdrop-blur-2xl">
+              <span>Agotado</span>
+            </div>
+          )}
           <img
             src={product.featuredImage.url ?? "/not-found.png"}
             alt={product.featuredImage.altText || product.title}
