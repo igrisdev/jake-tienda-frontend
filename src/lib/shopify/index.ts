f5ee09edf1835c965e2b16ea59d6c86f018b2ec2
igrisdev <johanalvarezz237@gmail.com>
2025-11-01 19:56:59 -0500

    Refactor revalidation function to include layout parameter for search path and clean up commented code

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -545,17 +545,60 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
   if (isCollectionUpdate) {
     revalidateTag(TAGS.collections);
     revalidatePath("/");
-    revalidatePath("/search");
+    revalidatePath("/search", "layout");
   }
+
   if (isProductUpdate) {
     revalidateTag(TAGS.products);
     revalidateTag(TAGS.collections);
     revalidatePath("/");
-    revalidatePath("/search");
+    revalidatePath("/search", "layout");
   }
 
   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
+
+// export async function revalidate(req: NextRequest): Promise<NextResponse> {
+//   const topic = req.headers.get("x-shopify-topic") || "unknown";
+//   const secret = req.nextUrl.searchParams.get("secret");
+
+//   const collectionWebhooks = [
+//     "collections/create",
+//     "collections/delete",
+//     "collections/update",
+//   ];
+//   const productWebhooks = [
+//     "products/create",
+//     "products/delete",
+//     "products/update",
+//   ];
+
+//   const isCollectionUpdate = collectionWebhooks.includes(topic);
+//   const isProductUpdate = productWebhooks.includes(topic);
+
+//   if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+//     console.error("Invalid revalidation secret.");
+//     return NextResponse.json({ status: 200 });
+//   }
+
+//   if (!isCollectionUpdate && !isProductUpdate) {
+//     return NextResponse.json({ status: 200 });
+//   }
+
+//   if (isCollectionUpdate) {
+//     revalidateTag(TAGS.collections);
+//     revalidatePath("/");
+//     revalidatePath("/search");
+//   }
+//   if (isProductUpdate) {
+//     revalidateTag(TAGS.products);
+//     revalidateTag(TAGS.collections);
+//     revalidatePath("/");
+//     revalidatePath("/search");
+//   }
+
+//   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+// }
 
 export async function getPage(handle: string): Promise<Page> {
   const res = await shopifyFetch<ShopifyPageOperation>({
