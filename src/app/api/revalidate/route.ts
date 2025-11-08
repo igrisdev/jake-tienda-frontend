1af3ac2b6006b10c97be6a6a9a465d0c7a425455
igrisdev <johanalvarezz237@gmail.com>
2025-11-08 11:38:15 -0500

    intento 3

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
-import { revalidate } from "@/lib/shopify";
+import { revalidatePath, revalidateTag } from "next/cache";
+import { TAGS } from "@/lib/constants";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  return revalidate(req);
+  const topic = req.headers.get("x-shopify-topic") || "unknown";
+  const secret = req.nextUrl.searchParams.get("secret");
+
+  const collectionWebhooks = [
+    "collections/create",
+    "collections/delete",
+    "collections/update",
+  ];
+  const productWebhooks = [
+    "products/create",
+    "products/delete",
+    "products/update",
+  ];
+
+  const isCollectionUpdate = collectionWebhooks.includes(topic);
+  const isProductUpdate = productWebhooks.includes(topic);
+
+  if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+    console.error("Invalid revalidation secret.");
+    return NextResponse.json({ status: 200 });
+  }
+
+  if (!isCollectionUpdate && !isProductUpdate) {
+    return NextResponse.json({ status: 200 });
+  }
+
+  if (isCollectionUpdate) {
+    revalidateTag(TAGS.collections);
+    revalidatePath("/");
+    revalidatePath("/search", "layout");
+    revalidatePath("/search");
+  }
+
+  if (isProductUpdate) {
+    revalidateTag(TAGS.products);
+    revalidateTag(TAGS.collections);
+    revalidatePath("/");
+    revalidatePath("/search");
+    revalidatePath("/search", "layout");
+  }
+
+  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -436,50 +436,50 @@ export async function addToCart(
   return reshapeCart(res.body.data.cartLinesAdd.cart);
 }
 
-export async function revalidate(req: NextRequest): Promise<NextResponse> {
-  const topic = req.headers.get("x-shopify-topic") || "unknown";
-  const secret = req.nextUrl.searchParams.get("secret");
+// export async function revalidate(req: NextRequest): Promise<NextResponse> {
+//   const topic = req.headers.get("x-shopify-topic") || "unknown";
+//   const secret = req.nextUrl.searchParams.get("secret");
 
-  const collectionWebhooks = [
-    "collections/create",
-    "collections/delete",
-    "collections/update",
-  ];
-  const productWebhooks = [
-    "products/create",
-    "products/delete",
-    "products/update",
-  ];
+//   const collectionWebhooks = [
+//     "collections/create",
+//     "collections/delete",
+//     "collections/update",
+//   ];
+//   const productWebhooks = [
+//     "products/create",
+//     "products/delete",
+//     "products/update",
+//   ];
 
-  const isCollectionUpdate = collectionWebhooks.includes(topic);
-  const isProductUpdate = productWebhooks.includes(topic);
+//   const isCollectionUpdate = collectionWebhooks.includes(topic);
+//   const isProductUpdate = productWebhooks.includes(topic);
 
-  if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
-    console.error("Invalid revalidation secret.");
-    return NextResponse.json({ status: 200 });
-  }
+//   if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+//     console.error("Invalid revalidation secret.");
+//     return NextResponse.json({ status: 200 });
+//   }
 
-  if (!isCollectionUpdate && !isProductUpdate) {
-    return NextResponse.json({ status: 200 });
-  }
+//   if (!isCollectionUpdate && !isProductUpdate) {
+//     return NextResponse.json({ status: 200 });
+//   }
 
-  if (isCollectionUpdate) {
-    revalidateTag(TAGS.collections);
-    revalidatePath("/");
-    // revalidatePath("/search", "layout");
-    revalidatePath("/search");
-  }
+//   if (isCollectionUpdate) {
+//     revalidateTag(TAGS.collections);
+//     revalidatePath("/");
+//     revalidatePath("/search", "layout");
+//     revalidatePath("/search");
+//   }
 
-  if (isProductUpdate) {
-    revalidateTag(TAGS.products);
-    // revalidateTag(TAGS.collections);
-    revalidatePath("/");
-    revalidatePath("/search");
-    // revalidatePath("/search", "layout");
-  }
+//   if (isProductUpdate) {
+//     revalidateTag(TAGS.products);
+//     // revalidateTag(TAGS.collections);
+//     revalidatePath("/");
+//     revalidatePath("/search");
+//     revalidatePath("/search", "layout");
+//   }
 
-  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
-}
+//   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+// }
 
 export async function getPage(handle: string): Promise<Page> {
   const res = await shopifyFetch<ShopifyPageOperation>({
