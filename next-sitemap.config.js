80e38cdd1583a11d0f5b956b3a0cac8a48c4f3a6
igrisdev <johanalvarezz237@gmail.com>
2025-12-05 23:39:55 -0500

    Fix indentation for siteUrl in next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
- // siteUrl: 'http://localhost:3000', // Cámbialo cuando tengas tu dominio final
+ siteUrl: 'http://localhost:3000', // Cámbialo cuando tengas tu dominio final
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
